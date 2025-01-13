ff7c2b483f733f2b4cf4a0f6af21aee83de72cdc
HTH-0 <xogud1793@naver.com>
2025-01-13 17:34:04 +0900

    [Bronze V] Title: A+B - 7, Time: 224 ms, Memory: 18736 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();        //테스트 개수
+        for(int i = 1; i<=t; i++){     // 1부터 시작해서 t번만큼
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            System.out.printf("Case #%d: " + (a+b) +"\n",i);
+        }
+    }
+}
